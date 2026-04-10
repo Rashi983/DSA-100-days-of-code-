71318d761e641f0dd662501f2e8eb4f0ee4544b5
Rashi983 <rashi6083@gmail.com>
2026-04-10 12:24:13 +0530

    Add files via upload

---
+++ Q60.c
@@ -0,0 +1,46 @@
+/*Q:Validate Min-heap
+Problem Statement:
+Check whether a given binary tree satisfies the Min-Heap property.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal
+
+Output Format:
+- Print YES if valid Min-Heap, otherwise NO
+
+Example:
+Input:
+7
+1 3 5 7 9 8 10
+
+Output:
+YES
+
+Explanation:
+Each parent node must be smaller than its children*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int arr[n];
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+    
+    for (int i = 0; i <= (n - 2) / 2; i++) {
+        if (2 * i + 1 < n && arr[i] > arr[2 * i + 1]) {
+            printf("NO");
+            return 0;
+        }
+        if (2 * i + 2 < n && arr[i] > arr[2 * i + 2]) {
+            printf("NO");
+            return 0;
+        }
+    }
+    
+    printf("YES");
+    return 0;
+}
