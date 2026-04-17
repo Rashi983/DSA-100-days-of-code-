452bd2d02158c0da37d7f05e28c9f438446a7bf2
Rashi983 <rashi6083@gmail.com>
2026-04-17 23:43:09 +0530

    Add files via upload

---
+++ Q76.c
@@ -0,0 +1,70 @@
+/*Q:Count connected components
+Problem Statement
+Using DFS or BFS, count number of connected components.
+
+Input Format
+n m
+edges
+
+Output Format
+Number of connected components.
+
+Sample Input
+6 3
+1 2
+2 3
+5 6
+
+Sample Output
+3
+
+Explanation
+Components: {1,2,3}, {4}, {5,6}*/
+
+#include <stdio.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];
+int visited[MAX];
+int n;
+
+void dfs(int v) {
+    visited[v] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (adj[v][i] == 1 && visited[i] == 0) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int m, u, v;
+    int components = 0;
+
+    scanf("%d %d", &n, &m);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+        visited[i] = 0;
+    }
+
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (visited[i] == 0) {
+            dfs(i);
+            components++;
+        }
+    }
+
+    printf("%d\n", components);
+
+    return 0;
+}
