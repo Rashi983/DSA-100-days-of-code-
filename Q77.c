b739c67833c5827850aab9b43cd3b7e6649c83d4
Rashi983 <rashi6083@gmail.com>
2026-04-19 11:08:09 +0530

    Add files via upload

---
+++ Q77.c
@@ -0,0 +1,66 @@
+/*Q:Check if graph is connected
+Problem Statement
+Using BFS or DFS, check if the entire graph is connected.
+
+Input Format
+n m
+edges
+
+Output Format
+CONNECTED
+NOT CONNECTED
+
+Sample Input
+4 2
+1 2
+3 4
+
+Sample Output
+NOT CONNECTED*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int **adj;
+int *visited;
+int n;
+
+void dfs(int v) {
+    visited[v] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (adj[v][i] && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int m;
+    scanf("%d %d", &n, &m);
+
+    adj = (int **)malloc((n + 1) * sizeof(int *));
+    for (int i = 0; i <= n; i++) {
+        adj[i] = (int *)calloc(n + 1, sizeof(int));
+    }
+
+    visited = (int *)calloc(n + 1, sizeof(int));
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+
+    dfs(1);
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            printf("NOT CONNECTED");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED");
+    return 0;
+}
